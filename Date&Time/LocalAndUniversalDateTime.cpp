ed8895cc85d9d206b195c709854bb6902ae74481
Tabish Sheikh <tabishahmed123.ta@gmail.com>
2024-01-25 15:18:45 -0400

    Create LocalAndUniversalDateTime.cpp

---
+++ Date&Time/LocalAndUniversalDateTime.cpp
@@ -0,0 +1,5 @@
+/*
+Our planet is a sphere. It revolves round its axis. The Earth rotates towards the east. So the Sun rises at different times in different locations. The Earth rotates once in about 24 hours. Therefore, the world was divided into 24 time zones. In each time zone, there is a different local time. This local time is often further modified by the daylight saving.
+*/
+
+
